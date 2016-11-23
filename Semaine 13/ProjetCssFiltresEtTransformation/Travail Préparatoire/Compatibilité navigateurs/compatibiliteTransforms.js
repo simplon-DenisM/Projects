d02a4b7ffd230807ef5d0e65ececfc57e0d29c41
simplon-DenisM <denismuresan16@gmail.com>
2016-11-23 15:49:20 +0100

    Organised documents

--- Semaine 13/ProjetCssFiltresEtTransformation/Travail Préparatoire/Compatibilité navigateurs/compatibiliteTransforms.js
+++
@@ -1,8 +0,0 @@
-function apparaitre (navigateur) {
-var texte = document.getElementById(navigateur).style.display;
-  if(texte=='none') {
-    document.getElementById(navigateur).style.display='block';
-  } else {
-    document.getElementById(navigateur).style.display='none';
-  }
-}
